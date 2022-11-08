25ed0030c40376a5e9c03b9542765d311f2448b8
Tomasz Stojek <stojek604@gmail.com>
2022-11-08 14:30:42 -0800

    finished

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,7 @@ function read_display_Quote(){
 }
 read_display_Quote();
 
+
 function insertName(){
 // to check if the user is logged in:
  firebase.auth().onAuthStateChanged(user =>{
@@ -28,4 +29,30 @@ function insertName(){
  })
 }
 insertName();
+
+function populateCardsDynamically() {
+    let hikeCardTemplate = document.getElementById("hikeCardTemplate");
+    let hikeCardGroup = document.getElementById("hikeCardGroup");
+    
+    db.collection("hikes").get()
+        .then(allHikes => {
+            allHikes.forEach(doc => {
+                var hikeName = doc.data().name; //gets the name field
+                var hikeID = doc.data().code; //gets the unique ID field
+                var hikeLength = doc.data().length; //gets the length field
+                let testHikeCard = hikeCardTemplate.content.cloneNode(true);
+                testHikeCard.querySelector('.card-title').innerHTML = hikeName;     //equiv getElementByClassName
+                testHikeCard.querySelector('.card-length').innerHTML = hikeLength;  //equiv getElementByClassName
+                testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);//equiv getElementByTagName
+                testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;   //equiv getElementByTagName
+                hikeCardGroup.appendChild(testHikeCard);
+            })
+
+        })
+}
+populateCardsDynamically();
+
+function setHikeData(id){
+    localStorage.setItem ('hikeID', id);
+}
 
